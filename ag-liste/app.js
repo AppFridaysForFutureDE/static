b5c5000fe9f7bdc8b6745cab6137680f5157129e
pascaaaal <pascal.faude@gmail.com>
2021-09-12 20:18:35 +0200

    Fixed AG Site Design

--- ag-liste/app.js
+++ ag-liste/app.js
@@ -7,10 +7,11 @@ for (let index = 0; index < agList.length; index++) {
     //Füge das icon der AG hinzu
     const ag_icon_img = document.createElement('img')
         //Falls ein Icon-Typ spezifiziert wurde, verwende diesen, ansonsten verwende den Dummy
+    if(!element.getAttribute('icon-name')) continue;
     const icon_name = (element.getAttribute('icon-name') ? element.getAttribute('icon-name') : 'empty') + '.svg'
     ag_icon_img.src = "media/ag_icons/" + icon_name;
     ag_icon_img.classList.add("ag-icon")
-    element.appendChild(ag_icon_img);
+    element.children[0].insertAdjacentElement('afterbegin', ag_icon_img);
 
 } //Füge Icons zu den Kontaktdaten hinzu
 listOfLinks = document.getElementsByTagName('a')
