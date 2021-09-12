cae353524a08eeb415d1557c6f48229db4cfa990
jmueller95 <ju.mu@posteo.de>
2021-09-12 15:19:28 +0200

    Verschiebe AG-Icons nach unten

--- ag-seite/app.js
+++ ag-seite/app.js
@@ -9,7 +9,9 @@ for (let index = 0; index < agList.length; index++) {
         //Falls ein Icon-Typ spezifiziert wurde, verwende diesen, ansonsten verwende den Dummy
     const icon_name = element.getAttribute('icon-name') ? element.getAttribute('icon-name') + '.svg' : 'dummy.png'
     ag_icon_img.src = "media/ag_icons/" + icon_name;
+    ag_icon_img.classList.add("ag-icon")
     element.appendChild(ag_icon_img);
+
 } //Füge Icons zu den Kontaktdaten hinzu
 listOfLinks = document.getElementsByTagName('a')
 for (let index = 0; index < listOfLinks.length; index++) {
